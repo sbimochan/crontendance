f05378bf543e6b79702d4c9ce5eb52c5a8c20e80
Bimochan Shrestha <bmochan@gmail.com>
2022-12-20 20:16:50 +0545

    Initial commit

---
+++ main.js
@@ -0,0 +1,43 @@
+const token = process.env.vyagutaToken;
+const today = new Date();
+const isFriday = today.getDay() === 5;
+const location = isFriday ? "OFFICE" : "HOME";
+
+function dateFormat(date) {
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  const year = date.getFullYear();
+
+  return `${year}-${month}-${day}`;
+}
+
+const endpoint =
+  "https://attendance.vyaguta.lftechnology.com/api/leave/worklogs";
+const workDate = dateFormat(today);
+const headers = {
+  accept: "application/json, text/plain, */*",
+  "accept-language": "en-US,en;q=0.9",
+  authorization: `Bearer ${token}`,
+  "content-type": "application/json",
+  "sec-ch-ua":
+    '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
+  "sec-ch-ua-mobile": "?0",
+  "sec-ch-ua-platform": '"macOS"',
+  "sec-fetch-dest": "empty",
+  "sec-fetch-mode": "cors",
+  "sec-fetch-site": "same-site",
+};
+const options = {
+  method: "POST",
+  headers: headers,
+  body: JSON.stringify({ location, workDate }),
+  referrer: "https://vyaguta.lftechnology.com/",
+  referrerPolicy: "strict-origin-when-cross-origin",
+  mode: "cors",
+  credentials: "include",
+};
+try {
+  fetch(url, headers);
+} catch (error) {
+  console.log("error", error);
+}
