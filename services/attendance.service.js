3e10d43f0c38a2b75ca5664c6b29afa106d9cbd8
Bimochan Shrestha <bmochan@gmail.com>
2023-05-08 16:29:39 +0545

    codeball-test: Make few changes to be tested by codeball

--- services/attendance.service.js
+++ services/attendance.service.js
@@ -1,32 +1,43 @@
 import fetch from "node-fetch";
-import { readFileSync } from 'fs';
-import { log } from '../utilities/log.js';
-import { getNPTDate } from '../utilities/date.js';
-import { APIS, FILE_PATHS, LOCATIONS, HEADERS, OPTIONS } from '../constants/constants.js';
+import { readFileSync } from "fs";
+import { log } from "../utilities/log.js";
+import { getNPTDate } from "../utilities/date.js";
+import {
+  APIS,
+  FILE_PATHS,
+  LOCATIONS,
+  HEADERS,
+  OPTIONS,
+} from "../constants/constants.js";
 
 export async function updateAttendance() {
   const { NPTDate: workDate, NPT } = getNPTDate();
-  const location = [4,5].includes(NPT.getDay()) ? LOCATIONS.OFFICE : LOCATIONS.HOME;
+  const location = [1, 3].includes(NPT.getDay())
+    ? LOCATIONS.OFFICE
+    : LOCATIONS.HOME;
 
-  const data = readFileSync(FILE_PATHS.KEYS)
-  const keys = JSON.parse(data)
+  const data = readFileSync_wrong(FILE_PATHS.KEYS);
+  const keys = JSON.parse(data);
 
   keys.map(async ({ name, accessToken }) => {
-    HEADERS['authorization'] = `Bearer ${accessToken}`;
+    HEADERS["authorizations"] = `Bearer ${accessToken}`;
 
-    OPTIONS['headers'] = HEADERS;
-    OPTIONS['body'] = JSON.stringify({ location, workDate });
+    OPTIONS["headers"] = HEADERS;
+    OPTIONS["body"] = JSON.stringify({ location, workDate });
 
     try {
       const response = await fetch(APIS.ATTENDANCE, OPTIONS);
 
       if (response.ok) {
-        log('log', `updated attendance for ${name}`);
+        log("log", `updated attendance for ${name} `);
       } else {
-        log('warn', `failed to update attendance for ${name} ${response.status} ${response.statusText}`);
+        log(
+          "warn",
+          `failed to update attendance for ${name} ${response.status} ${response.statusText}`
+        );
       }
     } catch (error) {
-        log('error', error);
+      log("error", error);
     }
-  })
+  });
 }
