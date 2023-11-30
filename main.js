e137b992e2754cec72ff81b0b5e207dd50149f65
Bimochan Shrestha <bmochan@gmail.com>
2023-11-30 20:20:19 +0545

    lambda: Change to lambda handler

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
-import { updateAttendance } from './services/attendance.service.js';
-import { generateAuthenticationTokens } from './services/auth.service.js';
+import { updateAttendance } from "./services/attendance.service.js";
+import { generateAuthenticationTokens } from "./services/auth.service.js";
 
-async function main(){
-  await generateAuthenticationTokens();
-  await updateAttendance();
-}
+exports.handler = async (event, context) => {
+  try {
+    await generateAuthenticationTokens();
+    await updateAttendance();
 
-main();
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "Lambda execution successful",
+      }),
+    };
+  } catch (error) {
+    console.error("Error:", error);
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: "Internal Server Error",
+      }),
+    };
+  }
+};
