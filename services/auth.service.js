a3de14ea8ce33f27357562e027fd9f512cb7d01c
Bimochan Shrestha <bmochan@gmail.com>
2023-11-30 22:12:00 +0545

    lambda: AWS secret manager init

--- services/auth.service.js
+++ services/auth.service.js
@@ -1,38 +1,46 @@
 import fetch from "node-fetch";
-import { log } from '../utilities/log.js';
-import { writeFileSync, readFileSync } from 'fs';
-import { APIS, FILE_PATHS } from '../constants/constants.js';
+import { log } from "../utilities/log.js";
+import { writeFileSync, readFileSync } from "fs";
+import { APIS, FILE_PATHS } from "../constants/constants.js";
+import { getSecretsFromSecretManager } from "./secretManager.service.js";
 
 export async function generateAuthenticationTokens() {
-  const data = readFileSync(FILE_PATHS.KEYS);
-  const keys = JSON.parse(data);
+  const data = await getSecretsFromSecretManager();
+  const keys = JSON.parse(data)
 
-  const promises = keys.map(async ({ name, refreshToken, locations = [] }) => {
+  const promises = () => {
     try {
-      const response = await fetch(`${APIS.AUTH}?clientId=lms&token=${refreshToken}`);
+      const response = await fetch(
+        `${APIS.AUTH}?clientId=lms&token=${keys.refreshToken}`
+      );
 
       if (response.ok) {
-        const { data: { accessToken, refreshToken } } = await response.json();
-        log('log', `fetched new access token & refresh token for ${name}`);
+        const {
+          data: { accessToken, refreshToken },
+        } = await response.json();
+        log("log", `fetched new access token & refresh token for ${keys.name}`);
 
         return {
-          name,
+          name: keys.name,
           accessToken,
           refreshToken,
-          locations
+          locations: keys.locations,
         };
       } else {
-        log('warn', `failed to fetch access token & refresh token for ${name} ${response.status} ${response.statusText}`);
+        log(
+          "warn",
+          `failed to fetch access token & refresh token for ${name} ${response.status} ${response.statusText}`
+        );
       }
     } catch (error) {
-      log('error', error);
+      log("error", error);
     }
 
     return new Promise.resolve(null);
-  })
+  });
 
   const refreshedKeys = await Promise.all(promises);
-  const validRefreshedKeys = refreshedKeys.filter(key => !!key);
+  const validRefreshedKeys = refreshedKeys.filter((key) => !!key);
 
   writeFileSync(FILE_PATHS.KEYS, JSON.stringify(validRefreshedKeys, null, 2));
 }
