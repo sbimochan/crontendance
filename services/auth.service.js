398eeca8169a4c66592547fd956bf7e674115f28
Bimochan Shrestha <bmochan@gmail.com>
2023-11-30 23:04:49 +0545

    lambda: Make singular secrets

--- services/auth.service.js
+++ services/auth.service.js
@@ -2,45 +2,38 @@ import fetch from "node-fetch";
 import { log } from "../utilities/log.js";
 import { writeFileSync, readFileSync } from "fs";
 import { APIS, FILE_PATHS } from "../constants/constants.js";
-import { getSecretsFromSecretManager } from "./secretManager.service.js";
+import { getSecretsFromSecretManager, updateSecrets } from "./secretManager.service.js";
 
 export async function generateAuthenticationTokens() {
   const data = await getSecretsFromSecretManager();
   const keys = JSON.parse(data)
 
-  const promises = async () => {
-    try {
-      const response = await fetch(
-        `${APIS.AUTH}?clientId=lms&token=${keys.refreshToken}`
+  try {
+    const response = await fetch(
+      `${APIS.AUTH}?clientId=lms&token=${keys.refreshToken}`
+    );
+
+    if (response.ok) {
+      const {
+        data: { accessToken, refreshToken },
+      } = await response.json();
+      log("log", `fetched new access token & refresh token for ${keys.name}`);
+
+      return {
+        name: keys.name,
+        accessToken,
+        refreshToken,
+        locations: keys.locations,
+      };
+    } else {
+      log(
+        "warn",
+        `failed to fetch access token & refresh token for ${name} ${response.status} ${response.statusText}`
       );
-
-      if (response.ok) {
-        const {
-          data: { accessToken, refreshToken },
-        } = await response.json();
-        log("log", `fetched new access token & refresh token for ${keys.name}`);
-
-        return {
-          name: keys.name,
-          accessToken,
-          refreshToken,
-          locations: keys.locations,
-        };
-      } else {
-        log(
-          "warn",
-          `failed to fetch access token & refresh token for ${name} ${response.status} ${response.statusText}`
-        );
-      }
-    } catch (error) {
-      log("error", error);
     }
+  } catch (error) {
+    log("error", error);
+  }
 
-    return new Promise.resolve(null);
-  };
-
-  const refreshedKeys = await Promise.all(promises);
-  const validRefreshedKeys = refreshedKeys.filter((key) => !!key);
-
-  writeFileSync(FILE_PATHS.KEYS, JSON.stringify(validRefreshedKeys, null, 2));
-}
+  updateSecrets(data, accessToken, refreshToken)
+};
--- services/secretManager.service.js
+++ services/secretManager.service.js
@@ -4,7 +4,7 @@ import {
   GetSecretValueCommand,
 } from "@aws-sdk/client-secrets-manager";
 
-const secret_name = "crontendance";
+const secretName = "crontendance";
 
 const client = new SecretsManagerClient({
   region: "ap-southeast-2",
@@ -14,7 +14,7 @@ export async function getSecretsFromSecretManager() {
   try {
     const response = await client.send(
       new GetSecretValueCommand({
-        SecretId: secret_name,
+        SecretId: secretName,
         VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
       })
     );
@@ -24,5 +24,20 @@ export async function getSecretsFromSecretManager() {
     // For a list of exceptions thrown, see
     // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
     throw error;
+  }
+}
+
+export async function updateSecrets(secretObject, newKey, newValue) {
+  const params = {
+    SecretId: secretName,
+    SecretString: JSON.stringify({
+      ...secretObject,
+      [newKey]: newValue,
+    }),
+  };
+  try {
+    await client.updateSecret(params)
+  } catch (error) {
+    console.log(' error:', error);
   }
 }
